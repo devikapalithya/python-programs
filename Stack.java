3e8e49f4d95bb595df54bbbab15f924ff7981527
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2019-10-01 09:51:11 +0530

    added generic class

--- Stack.java
+++ Stack.java
@@ -46,4 +46,14 @@ public class Stack extends LinkedList {
 		}
 	}
 }
+public class PrintGeneric {
+ 
+    public static <T> void printAnyType(T x) {
+      System.out.println("I am a variable of " + x.getClass());
+      System.out.println("The parent of my " + x.getClass().getGenericSuperclass());
+      System.out.println(x);
+      System.out.println();
+    }
+  }
+
 
