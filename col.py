97d8016cbe43637760328af685f4bf702ce7cf86
Mustaq Ahammad <43813438+mustaqmustu@users.noreply.github.com>
2019-10-01 09:57:03 +0530

    added generic box

--- col.py
+++ col.py
@@ -5,3 +5,15 @@ source venv/bin/activate
 git clone git@github.com:<your github username>/colosseum.git
 cd colosseum
 
+public class Box<T> {
+ 
+  private T t;
+ 
+  public void set(T t) {
+    this.t = t;
+  }
+ 
+  public T get() {
+    return t;
+  }
+}
